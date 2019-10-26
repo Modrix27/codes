c95ecad824fe491628d342afcc2537fbe902ddab
Modrix27 <57049205+Modrix27@users.noreply.github.com>
2019-10-26 22:50:42 +0100

    Add files via upload
    Password validator is a program that validates passwords to match specific rules. For example, the minimum length of the password must be eight characters long and it should have at least one uppercase letter in it. 
    
    A valid password is the one that conforms to the following rules:
     - Minimum length is 5;
     - Maximum length is 10;
     - Should contain at least one number;
     - Should contain at least one special character (such as &, +, @, $, #, %, etc.);
     - Should not contain spaces.
    
    Examples:
    Input: "Sololearn"
    Output: false
    
    Input: "John Doe"
    Output: false
    
    Input: "$ololearn7"
    Output: true

---
+++ password_validator.java
@@ -0,0 +1,55 @@
+﻿import java.util.Scanner;
+public class password_validator {
+
+
+    public static void main(String[] args) {
+        Scanner f = new Scanner(System.in); 
+               
+        int i,ok1=0,ok2=0,j;
+        String specialCharacters = "&+@$#%";
+        
+        char[] a = specialCharacters.toCharArray();
+        String pass = f.nextLine();
+        char[] v = pass.toCharArray();
+        
+        if (v.length>10){
+            System.out.print("False");
+            System.exit(0);
+            }
+            
+        if (v.length<5)
+            {System.out.print("False");
+            System.exit(0);
+            }
+            
+        for(i=0; i<v.length; i++)
+            for(j=0; j<a.length; j++)
+                if(v[i]==a[j])
+                    ok1=1;
+                    
+        if(ok1==0)
+            {System.out.print("False");
+            System.exit(0);
+            }
+            
+        for(i=0; i<v.length; i++)
+            if(v[i]==32)
+                {System.out.print("False");
+                System.exit(0);
+                }
+                
+        for(i=0; i<v.length; i++)
+            if((v[i]>=48)&&(v[i]<=57))
+                ok2=1;
+                
+        if(ok2==0)
+            {System.out.print("False");
+            System.exit(0);
+            }
+            
+        System.out.println("True");
+        f.close();
+    }
+
+
+}
